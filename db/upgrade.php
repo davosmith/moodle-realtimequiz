2453167369ee34fff9a050bf7869687085da9147
Davo Smith <git@davosmith.co.uk>
2012-10-21 19:20:08 +0100

    CONTRIB-3975 Fix missing fields when upgrading from Moodle 1.9 version

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,10 +1,39 @@
 <?php
 
+defined('MOODLE_INTERNAL') || die();
+
 function xmldb_realtimequiz_upgrade($oldversion) {
-    global $CFG;
+    global $DB;
 
-    $result = true;
+    $dbman = $DB->get_manager();
 
-    return $result;
+    // Add fields that were missing in the Moodle 1.9 version of this plugin.
+    if ($oldversion < 2012102100) {
+
+        $table = new xmldb_table('realtimequiz');
+
+        $field = new xmldb_field('intro', XMLDB_TYPE_TEXT, null, null, null, null, null, 'name');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        $field = new xmldb_field('introformat', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, FORMAT_HTML, 'intro');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        $field = new xmldb_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'introformat');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        $field = new xmldb_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'timecreated');
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        upgrade_mod_savepoint(true, 2012102100, 'realtimequiz');
+    }
+
+    return true;
 }
-?>
