f2737c51b4b635d5908c1aa108efe98871871e0c
Davo Smith <github@davosmith.co.uk>
2013-07-25 18:49:21 +0100

    Update version number ready to deploy to Moodle.org

--- version.php
+++ version.php
@@ -9,9 +9,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version   = 2013011000;  // The current module version (Date: YYYYMMDDXX)
+$module->version   = 2013061200;  // The current module version (Date: YYYYMMDDXX)
 $module->requires  = 2010112400;  // Moodle 2.0 (or above)
 $module->cron      = 0;           // Period for cron to check this module (secs)
 $module->component = 'mod_realtimequiz';
 $module->maturity  = MATURITY_STABLE;
-$module->release   = '2.x (Build: 2013011000)';
+$module->release   = '2.x (Build: 2013061200)';
